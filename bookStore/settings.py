6e40260408ddfbf49b8d9adbe3079898d08b46ee
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-03 08:46:41 +0300

    setup database to use postgres

--- bookStore/settings.py
+++ bookStore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bookStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgrsql',
+        'NAME': 'bookstore',
+        'USER':'awadh-dev',
+        'PASSWORD': os.os.environ.get("PASSWORD"),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
