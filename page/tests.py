53f940e4985ecdb2b7a47531e38fb2c5e241d459
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-10 10:45:09 +0300

    add tests for the pages app

--- page/tests.py
+++ page/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 # Create your tests here.
+
+class TestHomePage(SimpleTestCase):
+
+
+
+  def setUp(self):
+    url = reverse('home')
+    self.response = self.client.get(url)
+
+
+
+  def test_simple_homepage_testcase(self):
+    self.assertTrue(self.response.status_code, 200)
+
+
+  def test_homepage_url_name(self):
+    self.assertTrue(self.response.status_code, 200)
+
+
+  def test_template_used(self):
+    self.assertTemplateUsed(self.response, 'home.html' )
+
+  def test_homepage_url_resolve_HomePageView(self):
+    view = resolve('/')
+    self.assertEqual(
+      view.func.__name__, HomePageView.as_view().__name__
+    )
