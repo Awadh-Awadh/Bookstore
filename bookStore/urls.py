cb9b86ca0ab9ee936b7b6db9f93d357399304ffa
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-11 21:33:26 +0300

    add login content

--- bookStore/urls.py
+++ bookStore/urls.py
@@ -18,5 +18,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+    # user management
+
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # local apps
+
     path('', include('page.urls'))
 ]
