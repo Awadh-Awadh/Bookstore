0a7c159a423cf45aa8cbbcb301664763f6c77bd3
Awadh-Awadh <awadhawadh84@yahoo.com>
2022-03-18 11:00:57 +0300

    add tests for users

--- users/tests.py
+++ users/tests.py
@@ -1,10 +1,14 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
 
 from django.contrib.auth import get_user_model
+from .views import SignupView
+from .forms import CustomUserCreationForm
 
 
 
 class TestModels(TestCase):
+
      
      def test_create_user(self):
          User = get_user_model()
@@ -34,3 +38,28 @@ class TestModels(TestCase):
           self.assertTrue(user.is_active)
           self.assertTrue(user.is_superuser)
           self.assertTrue(user.is_staff)
+
+class SignupPageTests(TestCase):
+
+  def setUp(self):
+     url = reverse('signup')
+     self.response = self.client.get(url)
+
+
+  def test_signup_template(self):
+    self.assertEqual(self.response.status_code, 200)
+    self.assertTemplateUsed(self.response, 'signup.html')
+    self.assertContains(self.response, 'Sign Up')
+
+
+  def test_creation_form(self):
+    form = self.response.context.get("form")
+    self.assertIsInstance(form, CustomUserCreationForm)
+    self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+  def test_signup_resolves_SignupView(self):
+    view = resolve('/accounts/signup/')
+    self.assertEqual(
+      view.func.__name__, SignupView.as_view().__name__
+    )
+    
